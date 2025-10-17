f0c2ef7b63925ff80ef273f34162305e3b651935
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-17 16:26:05 +0900

    test: 사용자 입력 검증 기능 테스트 추가

---
+++ src/test/java/calculator/ValidatorTest.java
@@ -0,0 +1,90 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class ValidatorTest {
+
+    @Test
+    @DisplayName("기본 구분자 테스트")
+    void basicDelimiterTest() {
+        String input = "1,2,3";
+
+        assertThatCode(() -> Validator.validate(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("기본 구분자가 두 개일 때")
+    void basicDelimitersTest() {
+        String input = "1,2:3";
+
+        assertThatCode(() -> Validator.validate(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 테스트")
+    void customDelimiterTest() {
+        String input = "//;\n1;2;3";
+
+        assertThatCode(() -> Validator.validate(input)).doesNotThrowAnyException();
+    }
+    @Test
+    @DisplayName("커스텀 구분자와 기본 구분자 같이 사용")
+    void basicAndCustomDelimiterTest() {
+        String input = "//;\n1;2,3";
+
+        assertThatCode(() -> Validator.validate(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 숫자일 때")
+    void customDelimiterIsNumberTest() {
+        String input = "//4\n14243";
+
+        assertThatCode(() -> Validator.validate(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 경우 IllegalArgumentException")
+    void notNumberTest() {
+        String input = "1,a,3";
+
+        assertThatThrownBy(() -> Validator.validate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구분자가 연속으로 나오는 경우 IllegalArgumentException")
+    void consecutiveDelimiterTest() {
+        String input = "1,,2";
+
+        assertThatThrownBy(() -> Validator.validate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("음수가 있는 경우 IllegalArgumentException")
+    void negativeNumberTest() {
+        String input = "1,-2,3";
+
+        assertThatThrownBy(() -> Validator.validate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 2자 이상인 경우 IllegalArgumentException")
+    void longCustomDelimiterTest() {
+        String input = "//;;\n1;;2;;3";
+
+        assertThatThrownBy(() -> Validator.validate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 형식이 잘못된 경우 IllegalArgumentException")
+    void CustomDelimiterFormatIsInvalidTest() {
+        String input = "//;/n1;2,3";
+
+        assertThatThrownBy(() -> Validator.validate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
