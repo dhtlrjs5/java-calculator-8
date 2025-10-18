5848e8075ef1505560493a632b34965211f30e9d
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-18 10:58:55 +0900

    feat: 구분자를 기준으로 문자열 분리 기능 추가

---
+++ src/main/java/calculator/InputStringParser.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputStringParser {
+
+    public static List<Integer> parseString(String str, String delimiter) {
+
+        String[] tokens = str.split(delimiter);
+        List<Integer> list = new ArrayList<>();
+
+        for (String token : tokens) {
+            list.add(Integer.parseInt(token));
+        }
+
+        return list;
+    }
+}
